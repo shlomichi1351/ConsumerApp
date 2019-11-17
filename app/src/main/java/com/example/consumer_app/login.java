13016e9d23a4b18a929304b88e6908f07855bbfd
shlomichi1351 <shlomichi1351gmail.com>
2019-11-17 23:21:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/consumer_app/login.java
@@ -0,0 +1,96 @@
+package com.example.consumer_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.opengl.Visibility;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.Calendar;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+        TextView textView=findViewById(R.id.sup);
+        textView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent it = new Intent(login.this, signup.class);
+                startActivity(it);
+            }
+        });
+
+
+        ImageView imageView =findViewById(R.id.img);
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+        Glide.with(this).load(R.drawable.day).into(imageView);
+        if (timeOfDay >= 0 && timeOfDay < 12) {
+            Toast.makeText(this, "Good Morning", Toast.LENGTH_SHORT).show();
+           // Glide.with(this).load(R.drawable.day).into(imageView);
+        } else if (timeOfDay >= 12 && timeOfDay < 16) {
+            Toast.makeText(this, "Good Afternoon", Toast.LENGTH_SHORT).show();
+           // Glide.with(this).load(R.drawable.day).into(imageView);
+        } else if (timeOfDay >= 16 && timeOfDay < 21) {
+            Toast.makeText(this, "Good Evening", Toast.LENGTH_SHORT).show();
+           // Glide.with(this).load(R.drawable.night).into(imageView);
+        } else if (timeOfDay >= 21 && timeOfDay < 24) {
+            Toast.makeText(this, "Good Night", Toast.LENGTH_SHORT).show();
+           // Glide.with(this).load(R.drawable.night).into(imageView);
+        }
+
+        final TextView pss= findViewById(R.id.pswd);
+        final TextView sendpss=findViewById(R.id.sendpassword);
+        sendpss.setTypeface(null, Typeface.BOLD);
+        //sendpss.setTextColor(Color.parseColor("#888888"));
+
+
+        sendpss.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+            pss.setVisibility(View.VISIBLE);
+            //btn.setText("Sign In!");
+            new CountDownTimer(30000, 1000) {
+                public void onTick(long millisUntilFinished)
+                {
+                    //remember to check what the user choose!!!!!!!!!!!
+                    sendpss.setEnabled(false);
+                    sendpss.setText("We've sent you Email/SMS with the password.\nPlease enter the code below.\nYou will be able to request the password again in " + millisUntilFinished / 1000);
+                }
+
+                public void onFinish()
+                {
+                    sendpss.setEnabled(true);
+                    sendpss.setText("send me the password again!");
+                }
+            }.start();
+            }
+        });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/consumer_app/signup.java
@@ -0,0 +1,44 @@
+package com.example.consumer_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        getSupportActionBar().hide();
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+
+//        TextView textView=findViewById(R.id.sing);
+//        textView.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                Intent it = new Intent(signup.this, login.class);
+//                startActivity(it);
+//            }
+//        });
+
+//        Button btn=findViewById(R.id.sinnp);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+    }
+}
