5f4f0cad5fe255bd7aa80839586cae8cda1d1af7
shlomichi1351 <shlomichi1351gmail.com>
2019-11-17 23:24:26 +0200

    Start the project

--- app/src/main/java/com/example/consumer_app/signup.java
+++ app/src/main/java/com/example/consumer_app/signup.java
@@ -11,7 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class signup extends AppCompatActivity {
-
+// test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
